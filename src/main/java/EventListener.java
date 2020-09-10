34971888fec048dc2e8c570f837aae449a578ed0
Moritz <moripra@gmx.de>
2020-09-10 17:31:28 +0200

    uploading to mainframe... 100% done

--- src/main/java/EventListener.java
+++ src/main/java/EventListener.java
@@ -5,7 +5,10 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 public class EventListener extends ListenerAdapter {
     @Override
     public void onPrivateMessageReceived(PrivateMessageReceivedEvent event){
+        if (event.getAuthor().equals(MainBot.jda.getSelfUser()))
+            return;
         event.getChannel().sendMessage("Hallo du hurensohn").queue();
+
     }
 
 }
--- src/main/java/MainBot.java
+++ src/main/java/MainBot.java
@@ -10,6 +10,7 @@ import javax.security.auth.login.LoginException;
 public class MainBot {
 
     public static final String BOT_TOKEN = "NzUzNjEzMjM5MjcwNTA2NTE3.X1ovBQ.Y9LPSiKGzF6Opt44G9wu-mKJMBI";
+    public static JDA jda;
 
     public static void main(String[] args) {
 
@@ -20,7 +21,7 @@ public class MainBot {
             builder.setActivity(Activity.watching("you"));
             builder.setChunkingFilter(ChunkingFilter.ALL);
             builder.setMemberCachePolicy(MemberCachePolicy.ALL);
-            JDA jda = builder.build();
+            jda = builder.build();
             jda.awaitReady();
             jda.addEventListener(new EventListener());
         } catch (LoginException | InterruptedException ignored){}
