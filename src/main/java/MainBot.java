72de27fa26c2b644e5110ee535847a2fe354b609
Moritz <moripra@gmx.de>
2020-09-10 16:13:46 +0200

    uploading to mainframe... 100% done

--- src/main/java/MainBot.java
+++ src/main/java/MainBot.java
@@ -1,2 +1,19 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+
 public class MainBot {
+
+    public static void main(String[] args) {
+
+        JDABuilder builder = JDABuilder.createDefault("NzUzNjEzMjM5MjcwNTA2NTE3.X1ovBQ.Y9LPSiKGzF6Opt44G9wu-mKJMBI");
+        try {
+            JDA jda = builder.build();
+        } catch (LoginException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
