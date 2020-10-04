64d01dadce8fa93bd80a96fa94f7aa07eaec5b3c
Moritz <moripra@gmx.de>
2020-10-04 19:20:10 +0200

    Stundenplan 1.0 beta

--- src/main/java/Ausgaben.java
+++ src/main/java/Ausgaben.java
@@ -6,8 +6,6 @@ import java.io.File;
 
 public class Ausgaben {
 
-    //public static String resources = System.getProperty("user.dir");
-
     public Ausgaben(){
 
 
@@ -15,11 +13,11 @@ public class Ausgaben {
 
     public static Message stundenplan(User user){
 
+        File filePNG = new File("//var//lib//jenkins//workspace//MoritzUnibot//src//main//resources//Files//Fachsemester_1_Stundenplan.PNG");
+        File filepdf = new File("//var//lib//jenkins//workspace//MoritzUnibot//src//main//resources//Files//Fachsemester_1_Stundenplan.pdf");
 
-        File file = new File("//var//lib//jenkins//workspace//MoritzUnibot//src//main//resources//Files//Fachsemester_1_Stundenplan.pdf");
-
-        //user.openPrivateChannel().queue(channel -> channel.sendMessage("anfang " + resources + " ende").queue());
-        user.openPrivateChannel().queue(channel -> channel.sendFile(file, file.getName()).queue());
+        user.openPrivateChannel().queue(channel -> channel.sendFile(filepdf, filepdf.getName()).queue());
+        user.openPrivateChannel().queue(channel -> channel.sendFile(filePNG, filePNG.getName()).queue());
 
         return null;
     }
