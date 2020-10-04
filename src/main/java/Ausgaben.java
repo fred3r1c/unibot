bb7d0edd018be97a8497ba912762c83d28200dc9
Moritz <moripra@gmx.de>
2020-10-04 18:22:47 +0200

    EventListener upgedatet Stundenplan 0.2

--- src/main/java/Ausgaben.java
+++ src/main/java/Ausgaben.java
@@ -10,7 +10,7 @@ public class Ausgaben {
 
         File file = new File(System.getProperty("user.dir") + "\\\\resources//Files//Fachsemester_1_Stundenplan.PNG"); //src/main/resources/Files/Fachsemester_1_Stundenplan.PNG
 
-        user.openPrivateChannel().queue(channel -> channel.sendMessage(new MessageBuilder().append(file).build()).queue());
+        user.openPrivateChannel().queue(channel -> channel.sendFile(file, file.getName()).queue());
 
         return null;
     }
