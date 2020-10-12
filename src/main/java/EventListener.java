3212977cf58baa2ebf51c38bc26675bf8858c9a5
Moritz <moripra@gmx.de>
2020-10-12 18:32:54 +0200

    Rolenzuweiseung, custom emoji Test 2

--- src/main/java/EventListener.java
+++ src/main/java/EventListener.java
@@ -74,7 +74,7 @@ public class EventListener extends ListenerAdapter {
             return;
 
         if (event.getTextChannel().getId().equals("764936089001132072"))
-            System.out.println(event.getReactionEmote().getEmoji());
+            System.out.println(event.getReactionEmote().getEmote());
 
         /*if (!event.getTextChannel().getId().equals("764937703691124736"))
             return;
